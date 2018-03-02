2609fb0d96ec7b5782e9290733f4d2166407e639
Patrick Burns <pburns14@qub.ac.uk>
2018-03-02 17:36:33 +0000

    Replace Board.java

--- src/monoversity/Board.java
+++ src/monoversity/Board.java
@@ -27,16 +27,18 @@ public class Board {
 	 */
 	public void Play() {
 		// populating board square data
-		ISquare go = new GoSquare("Go", 1);
-		ISquare csb = new PropertySquare("CSB", 120, 24, 2,1);
-		ISquare dkb = new PropertySquare("DKB", 130, 26, 3,2);
-		ISquare pec = new PropertySquare("PEC", 140, 28, 4,3);
-		ISquare fps = new FreeParking("Free Parking", 5);
-		ISquare bot = new PropertySquare("The Bot", 190, 38, 6,4);
-		ISquare hat = new PropertySquare("The Hatfield", 200, 40, 7,5);
-		ISquare pc = new PubCrawl("Pubcrawl", 8);
+		Square go = new GoSquare("Go", 1);
+		Square csb = new PropertySquare("CSB", 2, 120, 24);
+		Square dkb = new PropertySquare("DKB", 3, 130, 26);
+		Square pec = new PropertySquare("PEC", 4, 140, 28);
+		Square fps = new FreeParking("Free Parking", 5);
+		Square bot = new PropertySquare("Bot", 6, 190, 38);
+		Square hat = new PropertySquare("Hatfield", 7, 200, 40);
+		Square pc = new PubCrawl("Pubcrawl", 8);
+		
+	
 
-		ISquare[] squares = { go, csb, dkb, pec, fps, bot, hat, pc };
+		Square[] squares = { go, csb, dkb, pec, fps, bot, hat, pc };
 
 		// setting initial var values for Player Turn
 
@@ -77,12 +79,12 @@ public class Board {
 		} while (sameNumber);
 
 		if (d1.getDiceNumber() > d2.getDiceNumber()) {
-			System.out.println(p1.playerName + " you got the highest value and will begin the game first");
+			System.out.println(p1.playerName + " you got the highest value and will begin the game first\n");
 		} else {
 			p2.setPlayerName(p1.getPlayerName());
 			p1.setPlayerName(p2.getPlayerName());
 
-			System.out.println(p1.playerName + " got the highest value and will now begin the game first as player 1");
+			System.out.println(p1.playerName + " got the highest value and will now begin the game first as player 1\n");
 		}
 
 		p1.setCurrentLocation(1);
@@ -95,12 +97,12 @@ public class Board {
 		do {
 
 			if (isPlayerOneTurn == true && isPlayerTwoTurn == false) {
-				System.out.printf("Player One: %s, your turn.\n", p1.getPlayerName());
-				System.out.println(p1.getPlayerName() + "  £" + p1.getPlayerBalance());
+				System.out.printf("Player One: %s, your turn.\n\n", p1.getPlayerName());
+				System.out.println(p1.getPlayerName() + " your current balance is : £" + p1.getPlayerBalance());
 
 				d1.throwDice();
 				p1.calculatePlayerLoc(d1.getDiceNumber(), p1.getCurrentLocation());
-				System.out.println("Loc Calculator test : " + p1.getCurrentLocation());
+				//System.out.println("Loc Calculator test : " + p1.getCurrentLocation());
 				p1.setCurrentLocation(p1.getCurrentLocation());
 
 				outputDetailsSquareLandedOn(p1, squares, p1.getCurrentLocation());
@@ -110,12 +112,12 @@ public class Board {
 
 			} else if (isPlayerTwoTurn == true && isPlayerOneTurn == false) {
 
-				System.out.printf("Player Two: %s, your turn.\n", p2.getPlayerName());
-				System.out.println(p2.getPlayerName() + "  £" + p2.getPlayerBalance());
+				System.out.printf("Player Two: %s, your turn.\n\n", p2.getPlayerName());
+				System.out.println(p2.getPlayerName() + " your current balance is : £" + p2.getPlayerBalance());
 
 				d1.throwDice();
 				p2.calculatePlayerLoc(d1.getDiceNumber(), p2.getCurrentLocation());
-				System.out.println("Loc Calculator test : " + p2.getCurrentLocation());
+				//System.out.println("Loc Calculator test : " + p2.getCurrentLocation());
 				p2.setCurrentLocation(p2.getCurrentLocation());
 
 				outputDetailsSquareLandedOn(p2, squares, p2.getCurrentLocation());
@@ -154,8 +156,8 @@ public class Board {
 	 * @param squares
 	 * @param currentBoardLocation
 	 */
-	public static void outputDetailsSquareLandedOn(Player player, ISquare[] squares, int currentBoardLocation) {
-		for (ISquare s : squares) {
+	public static void outputDetailsSquareLandedOn(Player player, Square[] squares, int currentBoardLocation) {
+		for (Square s : squares) {
 			if (s.getBoardLocation() == currentBoardLocation) {
 				s.Modify(player);
 			}
